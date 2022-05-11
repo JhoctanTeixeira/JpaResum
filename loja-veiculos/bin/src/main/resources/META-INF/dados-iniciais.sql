6c01855524255491423fdf809f18436bb80564d6
Jhoctan Teixeira <80040630+JhoctanTeixeira@users.noreply.github.com>
2022-05-11 09:20:08 -0300

    Delete loja-veiculos directory

--- loja-veiculos/bin/src/main/resources/META-INF/dados-iniciais.sql
+++
@@ -1,8 +0,0 @@
-insert into tab_veiculo (codigo, fabricante, modelo, ano_fabricacao, ano_modelo, valor)
-values (1, 'Fiat', 'Toro', 2020, 2020, 10700);
-
-insert into tab_veiculo (codigo, fabricante, modelo, ano_fabricacao, ano_modelo, valor)
-values (2, 'Ford', 'Fiesta', 2019, 2019, 42000);
-
-insert into tab_veiculo (codigo, fabricante, modelo, ano_fabricacao, ano_modelo, valor)
-values (3, 'Vw', 'Gol', 2019, 2020, 35000);
